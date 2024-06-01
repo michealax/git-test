d6f6c0623cad9e40bb9d7ecda1bbc27123f3fa50
shane <michealax@github.com>
2024-06-01 18:34:11 +0800

    g

--- app/NewLine.java
+++ app/NewLine.java
@@ -3,5 +3,6 @@ import org.w3c.dom.ls.LSOutput;
 public class NewLine {
     public static void main(String[] args) {
         System.out.println("new Line");
+        System.out.println("g");
     }
 }
